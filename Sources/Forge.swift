c7daae9b6c552294a9e73598e1e536afaad6299e
Alexsander Akers <me@a2.io>
2016-02-16 22:16:09 +0100

    Change get/set to from/to; move ForgeLens into separate file

--- Sources/Forge.swift
+++ Sources/Forge.swift
@@ -10,8 +10,8 @@ public struct File {
 }
 
 public struct Lens<ObjectType, PropertyType> {
-  let get: ObjectType -> PropertyType
-  let set: (PropertyType, ObjectType) -> ObjectType
+  let from: ObjectType -> PropertyType
+  let to: (PropertyType, ObjectType) -> ObjectType
 }
 
 public typealias PluginParameterType = ([File], Forge)
@@ -50,112 +50,22 @@ public struct Forge {
 
 extension Forge {
   public func use(plugin: Plugin) -> Forge {
-    let lens = Forge.pluginsLens
-    return lens.set(lens.get(self) + [plugin], self)
+    let lens = ForgeLens.plugins
+    return lens.to(lens.from(self) + [plugin], self)
   }
 
   public func ignore(paths: [Path]) -> Forge {
-    let lens = Forge.ignoreLens
-    return lens.set(lens.get(self) + paths, self)
+    let lens = ForgeLens.ignores
+    return lens.to(lens.from(self) + paths, self)
   }
 
   public func clean(shouldClean: Bool) -> Forge {
-    let lens = Forge.shouldCleanLens
-    return lens.set(shouldClean, self) 
+    let lens = ForgeLens.shouldClean
+    return lens.to(shouldClean, self)
   }
 
   public func frontmatter(parseFrontmatter: Bool) -> Forge {
-    let lens = Forge.parsesFrontmatterLens
-    return lens.set(parseFrontmatter, self)
+    let lens = ForgeLens.parsesFrontmatter
+    return lens.to(parseFrontmatter, self)
   }
-}
-
-extension Forge {
-  public static let containerLens = Lens<Forge, Path>(get: { $0.container }, set: { container, forge -> Forge in
-    return Forge(container: container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let sourceLens = Lens<Forge, Path>(get: { $0.source }, set: { source, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let destinationLens = Lens<Forge, Path>(get: { $0.destination }, set: { destination, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let pluginsLens = Lens<Forge, [Plugin]>(get: { $0.plugins }, set: { plugins, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let ignoresLens = Lens<Forge, [Path]>(get: { $0.ignores }, set: { ignores, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let contextLens = Lens<Forge, [Yaml : Yaml]>(get: { $0.context }, set: { context, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let shouldCleanLens = Lens<Forge, Bool>(get: { $0.shouldClean }, set: { shouldClean, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: shouldClean,
-                 parsesFrontmatter: forge.parsesFrontmatter)
-  })
-
-  public static let parsesFrontmatterLens = Lens<Forge, Bool>(get: { $0.parsesFrontmatter }, set: { parsesFrontmatter, forge -> Forge in
-    return Forge(container: forge.container,
-                 source: forge.source,
-                 destination: forge.destination,
-                 plugins: forge.plugins,
-                 ignores: forge.ignores,
-                 context: forge.context,
-                 shouldClean: forge.shouldClean,
-                 parsesFrontmatter: parsesFrontmatter)
-  })
 }
---
+++ Sources/ForgeLens.swift
@@ -0,0 +1,92 @@
+import PathKit
+import Yaml
+
+public struct ForgeLens {
+  public static let container = Lens<Forge, Path>(from: { $0.container }, to: { container, forge -> Forge in
+    return Forge(container: container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let source = Lens<Forge, Path>(from: { $0.source }, to: { source, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let destination = Lens<Forge, Path>(from: { $0.destination }, to: { destination, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let plugins = Lens<Forge, [Plugin]>(from: { $0.plugins }, to: { plugins, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let ignores = Lens<Forge, [Path]>(from: { $0.ignores }, to: { ignores, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let context = Lens<Forge, [Yaml : Yaml]>(from: { $0.context }, to: { context, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let shouldClean = Lens<Forge, Bool>(from: { $0.shouldClean }, to: { shouldClean, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: shouldClean,
+                 parsesFrontmatter: forge.parsesFrontmatter)
+  })
+
+  public static let parsesFrontmatter = Lens<Forge, Bool>(from: { $0.parsesFrontmatter }, to: { parsesFrontmatter, forge -> Forge in
+    return Forge(container: forge.container,
+                 source: forge.source,
+                 destination: forge.destination,
+                 plugins: forge.plugins,
+                 ignores: forge.ignores,
+                 context: forge.context,
+                 shouldClean: forge.shouldClean,
+                 parsesFrontmatter: parsesFrontmatter)
+  })
+}
