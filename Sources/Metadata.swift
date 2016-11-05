a24babc365902c554e060c94a1f2d35e35dca156
Alexsander Akers <me@a2.io>
2016-11-05 22:39:05 +0100

    Change Metadata to use ObjectIdentifier of key
    Use ObjectIdentifier instead of auto-incrementing numeric identifier.

--- Sources/Metadata.swift
+++ Sources/Metadata.swift
@@ -1,27 +1,19 @@
-import Foundation
-
-private var MetadataKeyCounter: Int64 = 0
-
 public final class MetadataKey<T> {
-    fileprivate let id: Int64
-
-    public init() {
-        self.id = OSAtomicIncrement64Barrier(&MetadataKeyCounter)
-    }
+    public init() {}
 }
 
 public struct Metadata {
-    private var storage: [Int64: Any]
+    private var storage: [ObjectIdentifier: Any]
 
     public init() {
         self.storage = [:]
     }
 
     public func get<T>(key: MetadataKey<T>) -> T? {
-        return storage[key.id] as? T
+        return storage[ObjectIdentifier(key)] as? T
     }
 
     public mutating func set<T>(key: MetadataKey<T>, value: T?) {
-        storage[key.id] = value
+        storage[ObjectIdentifier(key)] = value
     }
 }
